ae703de155eb85b9ad2beb33b1738f34be23a9c2
Erik P <erikvip@gmail.com>
2020-12-18 00:20:21 -0800

    Timezone, show title, and fix sqlite escaping

--- krcl-buffer.sh
+++ krcl-buffer.sh
@@ -1,32 +1,61 @@
-#!/bin/bash -ue
+#!/bin/bash -uex
+
+# Since this all happens in Salt Lake, keep everything on Mountain time for consistency.
+TZ="America/Denver";
+
 
 default_sleep_timer=60; 
 sleep_timer=$default_sleep_timer; 
 max_timer=600; 
 
-if [[ ! `pgrep streamripper` ]]; then
-	streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/%d.mp3' 2>&1 >> data/streamripper.log &
-fi
 
-while [ true ]; do
-	./update-krcl-playlist.sh;
-	song_end=$(sqlite3 db/krcl-playlist.sqlite3 'select end from playlist order by start desc limit 1' );
-	song_end_ts=$(date -d "${song_end}" "+%s");
-#	./now-playing.sh
-	now_ts=$(date "+%s");
-#	echo "$song_end_ts - $now_ts";
-	sleep_timer=$(( $song_end_ts - $now_ts ));
-
-	./now-playing.sh
-	echo "Interval: ${sleep_timer}";
-
-	if [ $sleep_timer -lt 1 -o $sleep_timer -gt $max_timer ]; then
-		sleep_timer=$default_sleep_timer;
+setup() {
+	if [[ ! `pgrep streamripper` ]]; then
+		#TZ="America/Denver" streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/%d.mp3' 2>&1 >> data/streamripper.log &
+		streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/%d.mp3' 2>&1 >> data/streamripper.log &
 	fi
+}
 
-	echo "Sleeping for ${sleep_timer} seconds...";
+main() { 
+	while [ true ]; do
+		./update-krcl-playlist.sh;
+		song_end=$(TZ="America/Denver" sqlite3 db/krcl-playlist.sqlite3 'select end from playlist order by start desc limit 1' );
+		song_end_ts=$(TZ="America/Denver" date -d "${song_end}" "+%s");
+		now_ts=$(TZ="America/Denver" date "+%s");
+		sleep_timer=$(( $song_end_ts - $now_ts ));
 
-	sleep $sleep_timer;
-done;
+		./now-playing.sh
+#		echo "Interval: ${sleep_timer}";
+
+		if [ $sleep_timer -lt 1 -o $sleep_timer -gt $max_timer ]; then
+			sleep_timer=$default_sleep_timer;
+		fi
+
+#		echo "Sleeping for ${sleep_timer} seconds...";
+#		sleep $sleep_timer;
+		sleepdisplay $sleep_timer;
+
+	done;
+}
+
+
+sleepdisplay() {
+	_timeout=$1;
+	_remaining=$_timeout;
+
+	while [ $_remaining -gt 0 ]; do
+		echo -ne "\rSleeping for $_remaining seconds...";
+		_remaining=$(( $_remaining - 1 ));
+		sleep 1
+		echo -ne "\033[2K"
+	done
+	
+	
+
+}
+
+
+setup
+main
 	
 
--- update-krcl-playlist.sh
+++ update-krcl-playlist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 wget --user-agent Firefox -q -O - 'https://krcl.org/endpoints/now-playing/' | \
 	gunzip | \
-	jq -r '.now_playing | "REPLACE INTO playlist (id, start, end, artist, release, song, duration) VALUES( \(.id|@sh), \(.start|@sh), \(.end|@sh), \(.artist|@sh), \(.release|@sh), \(.song|@sh), \(.duration|@sh) ); "  ' | sqlite3 db/krcl-playlist.sqlite3
+	jq -r '"REPLACE INTO playlist (id, start, end, artist, release, song, duration, showtitle) VALUES( \(.now_playing.id|@sh), \(.now_playing.start|@sh), \(.now_playing.end|@sh), \(.now_playing.artist|@sh), \(.now_playing.release|@sh), \(.now_playing.song|@sh), \(.now_playing.duration|@sh), \(.on_air_now.title|@sh) ); "  ' | \
+	sed "s#'\\\''#''#g" | \
+	sqlite3 db/krcl-playlist.sqlite3
+
+# The second to last sed line there converts shell escaping to Sqlite escaping...
+
 
 
