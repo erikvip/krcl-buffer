da693cf5782dde916d0aeb45de723189864593f1
Erik P <erikvip@gmail.com>
2020-12-18 01:12:12 -0800

    date / song index tagging

--- krcl-buffer.sh
+++ krcl-buffer.sh
@@ -1,24 +1,30 @@
-#!/bin/bash -uex
+#!/bin/bash -ue
 
 # Since this all happens in Salt Lake, keep everything on Mountain time for consistency.
 TZ="America/Denver";
 
-
 default_sleep_timer=60; 
 sleep_timer=$default_sleep_timer; 
 max_timer=600; 
-
+startup_date="";
+streamripper_date="";
+song_index=0;
 
 setup() {
-	if [[ ! `pgrep streamripper` ]]; then
-		#TZ="America/Denver" streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/%d.mp3' 2>&1 >> data/streamripper.log &
-		streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/%d.mp3' 2>&1 >> data/streamripper.log &
+	startup_date=$(TZ="America/Denver" date '+%Y%m%d');
+	song_index=0;
+
+	if [[ ! `pgrep --full 'streamripper.*krcl-high'` ]]; then
+		# Try to guess the streamripper file...
+		TZ="America/Denver" streamripper 'http://stream.xmission.com/krcl-high' -A -a 'data/new/%d.mp3' 2>&1 >> data/streamripper.log &
+		streamripper_date=$(TZ="America/Denver" date '+%Y_%m_%d_%H_%M_%S');
 	fi
 }
 
 main() { 
+	echo "Streamripper filename: ${streamripper_date}";
 	while [ true ]; do
-		./update-krcl-playlist.sh;
+		./update-krcl-playlist.sh ${song_index} ${streamripper_date};
 		song_end=$(TZ="America/Denver" sqlite3 db/krcl-playlist.sqlite3 'select end from playlist order by start desc limit 1' );
 		song_end_ts=$(TZ="America/Denver" date -d "${song_end}" "+%s");
 		now_ts=$(TZ="America/Denver" date "+%s");
@@ -35,6 +41,8 @@ main() {
 #		sleep $sleep_timer;
 		sleepdisplay $sleep_timer;
 
+		
+
 	done;
 }
 
@@ -47,13 +55,16 @@ sleepdisplay() {
 		echo -ne "\rSleeping for $_remaining seconds...";
 		_remaining=$(( $_remaining - 1 ));
 		sleep 1
-		echo -ne "\033[2K"
+		echo -ne "\033[2K\r"
 	done
-	
-	
-
 }
 
+finish() {
+	pkill --full "streamripper.*krcl-high";
+}
+
+
+trap finish EXIT
 
 setup
 main
--- update-krcl-playlist.sh
+++ update-krcl-playlist.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+_songindex="NULL";
+_streamfile="NULL";
+
+if [[ "$1" =~ ^[0-9]+ ]]; then
+	_songindex="${1}";
+	if [[ "$2" =~ [0-9\_]+ ]]; then
+		_streamfile="'${2}'";
+	fi
+fi
+
+
 wget --user-agent Firefox -q -O - 'https://krcl.org/endpoints/now-playing/' | \
 	gunzip | \
-	jq -r '"REPLACE INTO playlist (id, start, end, artist, release, song, duration, showtitle) VALUES( \(.now_playing.id|@sh), \(.now_playing.start|@sh), \(.now_playing.end|@sh), \(.now_playing.artist|@sh), \(.now_playing.release|@sh), \(.now_playing.song|@sh), \(.now_playing.duration|@sh), \(.on_air_now.title|@sh) ); "  ' | \
+	jq -r "\"REPLACE INTO playlist (id, start, end, artist, release, song, duration, showtitle, songindex, streamfile) VALUES( \(.now_playing.id|@sh), \(.now_playing.start|@sh), \(.now_playing.end|@sh), \(.now_playing.artist|@sh), \(.now_playing.release|@sh), \(.now_playing.song|@sh), \(.now_playing.duration|@sh), \(.on_air_now.title|@sh), ${_songindex}, ${_streamfile} ); \" " | \
 	sed "s#'\\\''#''#g" | \
 	sqlite3 db/krcl-playlist.sqlite3
 
